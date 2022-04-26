7f36d7d072973b9439c2ec76f75dfc117a1bdf51
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-04-26 19:20:23 +0200

    user.model.js with all of the parameters

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 require('dotenv').config({path: './config/.env'});
-require ('./config/db');
+require('./config/db');
 const app = express(); 
 
 app.listen(process.env.PORT, () => {
