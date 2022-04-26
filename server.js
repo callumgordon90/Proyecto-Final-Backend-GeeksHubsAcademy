3b4f63ff04d5c4656b9373924e54dd8ae2e39799
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-04-26 17:00:57 +0200

    created the .env and put the port number inside

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require ('express');
+require('dotenv').config({path: './config/.env'})
 const app = express(); 
 
-app.listen(5000, () => {
-    console.log('listening on port 5000');
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`);
 })
