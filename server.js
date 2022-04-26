81a75c843b665b84d09894197fd424eb16909190
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-04-26 19:31:29 +0200

    added the routers folder and the user.routes document

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require ('express');
+const userRoutes = require ('./routes/user.routes');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
 const app = express(); 
 
+
+
+
+
+//routes
+app.use('/api/user', userRoutes);
+
+// server
 app.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}`);
 })
