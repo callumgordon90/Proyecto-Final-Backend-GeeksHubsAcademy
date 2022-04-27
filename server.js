51fd7a3b2122bed6c769c08d45ffc4c05e83b7b6
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-04-27 15:41:56 +0200

    installed bodyParser and added it to server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require ('express');
+const bodyParser = require ('body-parser');
 const userRoutes = require ('./routes/user.routes');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
 const app = express(); 
 
 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 //routes
