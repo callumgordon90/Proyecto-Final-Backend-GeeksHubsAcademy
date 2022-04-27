297f88b46ebfe3662ec8df58954efa42c619a385
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-04-27 16:55:30 +0200

    added the user.controller.js file and a CRUD function to get all users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
+const userController = require("../controllers/user.controller");
 
+
+//authorisation (register/login/logout)
 router.post("/register" , authController.signUp);
+
+// user display: 'block' (show all of the users in the database),
+router.get('/',userController.getAllUsers);
 
 module.exports = router;
 
