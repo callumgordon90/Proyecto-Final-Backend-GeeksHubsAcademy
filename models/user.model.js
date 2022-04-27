1e42eef955c51438f146a6f318b4ad41c2aef85a
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-04-27 16:25:25 +0200

    installed bcrpyt and added a function in the user.model.js document to encrypt passwords

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,8 @@
 const mongoose = require ('mongoose');
 const {isEmail} = require ('validator');
+const bcrypt = require ('bcrypt');
+
+
 const userSchema = new mongoose.Schema (
     {
         pseudo: {
@@ -25,8 +28,8 @@ const userSchema = new mongoose.Schema (
         },
         picture: {
             type: String,
-            default: "./uploads/profile/random-user.png"
-        },
+            default: "Number./uploads/profile/random-user.png"
+        }
         bio:{
             type: String,
             max: 1024,
@@ -49,21 +52,11 @@ const userSchema = new mongoose.Schema (
 // play function before save into display: 'block',
 userSchema.pre("save", async function(next) {
     const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
+    this.password = await bcrypt.hash.apply(this.password, salt);
     next();
-  });
-  
-  userSchema.statics.login = async function(email, password) {
-    const user = await this.findOne({ email });
-    if (user) {
-      const auth = await bcrypt.compare(password, user.password);
-      if (auth) {
-        return user;
-      }
-      throw Error('incorrect password');
-    }
-    throw Error('incorrect email')
-  };
+});
+
+
   
   const UserModel = mongoose.model("user", userSchema);
   
