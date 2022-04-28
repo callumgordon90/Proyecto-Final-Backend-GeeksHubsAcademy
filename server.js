dbd05463325bf00b48d8f28ca12b39eee5d52e1c
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-04-28 14:19:08 +0200

    finished the middleware for the aithorisation process

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const userRoutes = require ('./routes/user.routes');
-require('dotenv').config({path: './config/.env'});
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const userRoutes = require('./routes/user.routes');
+require('dotenv').config({ path: './config/.env' });
+const { checkUser, requireAuth } = require('./middleware/auth.middleware');
+
 const dbconnect = require('./config/db');
-const app = express(); 
+const app = express();
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//JWT
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {
+    res.status(200).setDefaultEncoding(res.locals.user._id)
+})
 
 
 //routes
