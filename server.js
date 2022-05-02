f6f776303eefb413692f93727aa58720fe6e1cf5
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-05-02 16:57:56 +0200

    added CORS functionality to server.js and .env

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ const postRoutes = require('./routes/post.routes');
 require('dotenv').config({ path: './config/.env' });
 const { checkUser, requireAuth } = require('./middleware/auth.middleware');
 const dbconnect = require('./config/db');
+
+const cors = require('cors');
+
 const app = express();
 
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'methods': 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    'preflightContinue': false
+}
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
