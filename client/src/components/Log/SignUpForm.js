181c01319dfbab3a5b4aa2290fb48c5d7a60acf2
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-05-08 09:31:45 +0200

    basic template for the sign up form in react

--- client/src/components/Log/SignUpForm.js
+++ client/src/components/Log/SignUpForm.js
@@ -1,10 +1,68 @@
 import React from 'react';
+import axios from 'axios';
 
 const SignUpForm = () => {
+    const [pseudo, setPseudo] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [controlPassword, setControlPassword] = useState('');
+
+    const handleRegister = async (e) => {
+
+    }
+
     return (
-        <div>
-            Hello from sign up form
-        </div>
+        <form action="" onSubmit={handleRegister} id="sign-up-form">
+            <label htmlFor="pseudo">Pseudo</label>
+            <br />
+            <input
+                type="text"
+                name="pseudo"
+                id="pseudo"
+                onChange={(e) => setPseudo(e.target.value)}
+                value={pseudo}
+            />
+            <div className="pseudo error"></div>
+            <br />
+            <label htmlFor="email">Email</label>
+            <br />
+            <input
+                type="text"
+                name="email"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+            />
+            <div className="email error"></div>
+            <br />
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+                type="password"
+                name="password"
+                id="password"
+                onChange={(e) => setPassowrd(e.target.value)}
+                value={password}
+            />
+            <div className="password error"></div>
+            <br />
+            <label htmlFor="password-conf">Confirm password</label>
+            <input
+                type="password"
+                name="password"
+                id="password-conf"
+                onChange={(e) => setControlPassword(e.target.value)}
+                value={controlPassword}
+            />
+            <div className="password-confirm error"></div>
+            <br />
+            <input type="checkbox" id="terms" />
+            <label htmlFor="terms">I accept the <a href="/" target="_blank"
+                rel="noopener noreferrer">general conditions</a></label>
+            <div className="terms error"></div>
+            <br />
+            <input type="submit" value="Validate Registration" />
+        </form>
     );
 };
 
