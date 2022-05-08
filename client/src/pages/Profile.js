9b9fb79d0869e792e6b05df3d2689b2767f3b2e6
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-05-08 08:44:34 +0200

    modifiactios to SignUP, profile.js and created AppContext.js file

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Log from '../components/Log';
+import { UidContext } from '../components/AppContext';
 
 const Profile = () => {
+    const uid = useContext(UidContext);
+
     return (
-        <div className = "profile-page"> 
-            <div className = "log-container"> 
-                <Log signin={false} signup={true} />
-                <div className="img-container">
-                    <img src="./img/log.svg" alt="img-log"/>
+        <div className="profile-page">
+            {uid ? (
+                <h1>UPDATE PAGE</h1>
+            ) : (
+                <div className="log-container">
+                    <Log signin={false} signup={true} />
+                    <div className="img-container">
+                        <img src="./img/log.svg" alt="img-log" />
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 };
