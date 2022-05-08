0732fda8aeb720fd30e0c87f69032507c5df0982
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-05-08 21:34:28 +0200

    following popup in Updateprofile.js

--- client/src/components/Profile/UpdateProfile.js
+++ client/src/components/Profile/UpdateProfile.js
@@ -3,12 +3,15 @@ import LeftNav from '../LeftNav';
 import { useSelector } from "react-redux";
 import UploadImg from "./UploadImg";
 import { updateBio } from "../../actions/user.actions";
+import { dateParser } from "../tools";
 
 const UpdateProfile = () => {
     const [bio, setBio] = useState ('');
     const [updateForm, setUpdateForm] = useState(false);
     const userData = useSelector((state) => state.userReducer);
     const dispatch = useDispatch();
+    const [followingPopup, setFollowingPopup] = useState(false);
+    const [followersPopup, setFollowersPopup] = useState(false);
 
     const handleUpdate = ()=> {
         dispatch(updateBio(userData._id, bio));
@@ -47,8 +50,22 @@ const UpdateProfile = () => {
                         )}
                     </div>
                     <h4>Member since : {dateParser (userData.createdAt)}</h4>
+                    <h5 onClick={() => setFollowingPopup(true)}>Following : {userData.following ? userData.following.length : ""}</h5>
+                    <h5 onClick={() => setFollowersPopup(true)}>Followers : {userData.followers ? userData.followers.length : ""}</h5>
                 </div>
             </div>
+            {followingPopup && (
+                <div className="popup-profile-container">
+                    <div className="model">
+                        <h3>Following></h3>
+                        <span className="cross" onClick={() => setFollowingPopup(false)}>
+                        &#100005;
+                        </span>
+                        <ul>
+                            
+                        </ul>
+                    </div>
+                </div>)}
         </div>
     );
 };
