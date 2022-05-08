acf20135829329b5c1c98f0cbaf3f816a47b200d
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-05-08 13:02:10 +0200

    added UploadImg.js to the profile folder

--- client/src/components/Profile/UpdateProfile.js
+++ client/src/components/Profile/UpdateProfile.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import LeftNav from '../LeftNav';
+import { useSelector } from "react-redux";
 
 const UpdateProfile = () => {
+    const userData = useSelector((state) => state.userReducer)
     return (
         <div className='profile-container'>
             <LeftNav />
+            <h1> Profile page of {userData.pseudo}</h1>
+            <div className='update-container'>
+                <div className='left-part'>
+                    <h3>Profile photo</h3>
+                    <img src={userData.picture} alt="user-pic" />
+                    UPLOAD PIC
+                    <p>{errors.maxSize}</p>
+                    <p>{errors.format}</p>
+                </div>
+            </div>
         </div>
     );
 };
