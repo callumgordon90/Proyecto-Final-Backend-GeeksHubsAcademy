ca9b40b00c5be6b1d24f76e3577e17f2e07d7d35
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-05-10 22:10:20 +0200

    modified the home page and added a uid function

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UidContext } from '../components/AppContext';
 import LeftNav from '../components/LeftNav';
 import Thread from '../components/Thread';
 
 const Home = () => {
+    const uid = useContext(UidContext);
+
     return (
         <div className="home">
             <LeftNav />
             <div className="main">
+                <div className='home-header'>
+
+                </div>
                 <Thread />
             </div>
         </div>
