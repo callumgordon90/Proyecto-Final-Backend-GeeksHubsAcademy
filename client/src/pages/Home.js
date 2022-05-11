84f06526b561e81d7515591ff26c00e42516e7c8
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-05-11 19:40:16 +0200

    CRUD add post

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
 import { UidContext } from '../components/AppContext';
 import LeftNav from '../components/LeftNav';
+import NewPostForm from '../components/Post/NewPostForm';
 import Thread from '../components/Thread';
+import Log from '../components/Log';
 
 const Home = () => {
     const uid = useContext(UidContext);
@@ -11,7 +13,7 @@ const Home = () => {
             <LeftNav />
             <div className="main">
                 <div className='home-header'>
-
+                    {uid ? <NewPostForm /> : <Log signin={true} signup={false} />}
                 </div>
                 <Thread />
             </div>
