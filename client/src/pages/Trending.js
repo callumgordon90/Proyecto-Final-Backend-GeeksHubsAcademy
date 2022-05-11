1d5bd3aad12159c8a63778eb92d9d4b1b3ed2441
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-05-11 22:17:39 +0200

    trending.js return function

--- client/src/pages/Trending.js
+++ client/src/pages/Trending.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react-redux';
+import { UidContext } from "../components/AppContext";
+import LeftNav from '../components/LeftNav';
+import { isEmpty } from "../components/tools";
+import Card from "../components/Post/Card";
 
 const Trending = () => {
+    const uid = useContext(UidContext);
+    const trendList = useSelector((state) => state.trendingReducer);
+
+
     return (
-        <div>
-            Hello from trending
+        <div className='trending-page'>
+            <LeftNav />
+            <div className='main'>
+                <ul>
+                    {!isEmpty(trendList[0]) && trendingList.map((post) => <Card post={post}
+                    key={post._id} /> )}
+                </ul>
+            </div>
+            
         </div>
     );
 };
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,11 +4,13 @@ import usersReducer from "./users.reducer";
 import postReducer from "./post.reducer";
 import errorReducer from "./error.reducer";
 import allPostsReducer from "./allPosts.reducer";
+import trendingReducer from "./trending.reducer";
 
 export default combineReducers({
     userReducer,
     usersReducer,
     postReducer,
     errorReducer,
-    allPostsReducer
+    allPostsReducer,
+    trendingReducer
 });
