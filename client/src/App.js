34043ebfcb70f208edab477243df3af9b32592d8
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-05-15 18:55:02 +0200

    backend packages modified and downgraded to run backend correctly. changed package json and reinstalled package lock and node modules in the back end

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,25 @@
-import React, {useEffect, useState} from 'react';
-import Routes from "./components/Routes";
-import { UidContext } from './components/AppContext';
-import axios from "axios";
-import {useDispatch} from 'react-redux';
-import {getUser} from "./actions/user.actions";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  const [uid, setUid] = useState(null);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const fetchToken = async () => {
-      await axios({
-        method: "get",
-        url: `${process.env.REACT_APP_API_URL}jwtid`,
-        withCredentials: true
-      })
-        .then((res) => {
-          console.log(res);
-          setUid(res.data);
-        })
-        .catch((err) => console.log("No token"));
-    };
-    fetchToken();
-
-    if (uid) dispatch(getUser(uid));
-  }, [uid, dispatch]);
-
-
+function App() {
   return (
-    <UidContext.Provider value={uid}>
-      <Routes />
-    </UidContext.Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import './styles/index.scss';
-import {Provider} from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
-import { getUsers } from "./actions/users.actions";
+import reportWebVitals from './reportWebVitals';
 
-// dev tools
-import {composeWithDevTools} from 'redux-devtools-extension';
-import logger from 'redux-logger';
-import { getPosts } from './actions/post.actions';
-
-
-const store = createStore(
-  rootReducer, composeWithDevTools(applyMiddleware(thunk))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-store.dispatch(getUsers());
-store.dispatch(getPosts());
-
-
-ReactDOM.render(
-  <Provider store={store}>
-  <App />,
-  </Provider>,
-  document.getElementById('root')
-);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
