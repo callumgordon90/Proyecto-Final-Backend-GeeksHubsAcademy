dbe2c54270048f52ac5d66445c7520c07dbc963d
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-05-16 19:22:18 +0200

    updated all of the pages to fit with the old package json

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import Log from '../components/Log';
+import { UidContext } from '../components/AppContext';
+import UpdateProfile from '../components/Profile/UpdateProfile';
+
+const Profile = () => {
+    const uid = useContext(UidContext);
+
+    return (
+        <div className="profile-page">
+            {uid ? (
+                <UpdateProfile />
+            ) : (
+                <div className="log-container">
+                    <Log signin={false} signup={true} />
+                    <div className="img-container">
+                        <img src="./img/log.svg" alt="img-log" />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Profile;
