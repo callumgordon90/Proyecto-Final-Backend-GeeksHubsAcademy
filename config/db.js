b309e440ee4a7ff582213061d54aac0ad2b9e78f
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-06-02 22:00:39 +0200

    heroku changes

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 
-//Function to connect to the base:
+//Function to connect to the database:
 const dbconnect = () => {
     const mongoose = require("mongoose");
     console.log("mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.qi0fs.mongodb.net/mern-project");
