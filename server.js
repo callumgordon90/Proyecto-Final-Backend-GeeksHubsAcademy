085f84d01c1fa1f25e4365c9a5ddc9882c115bcd
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-06-02 20:39:23 +0200

    cors policy

--- server.js
+++ server.js
@@ -3,32 +3,34 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 const postRoutes = require('./routes/post.routes');
-require('dotenv').config({ path: './config/.env' });
-const { checkUser, requireAuth } = require('./middleware/auth.middleware');
-const dbconnect = require('./config/db');
+require('dotenv').config({path: './config/.env'});
+require('./config/db');
+const {checkUser, requireAuth} = require('./middleware/auth.middleware');
+
 
 const cors = require('cors');
 
 const app = express();
 
 const corsOptions = {
-     origin: '*',
-
-    'methods': 'GET, HEAD, PUT, PATCH, POST, DELETE'
-    
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
 }
 app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
-//JWT
+// JWT
 app.get('*', checkUser);
 app.get('/jwtid', requireAuth, (req, res) => {
-    res.status(200).send(res.locals.user._id)
-})
-
+  res.status(200).send(res.locals.user._id)
+});
 
 //connenction to the routes
 app.use('/api/user', userRoutes);
@@ -37,8 +39,7 @@ app.use('/api/post', postRoutes);
 //function to connect to database:
 dbconnect();
 
-
 // server
 app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`);
+  console.log(`Listening on port ${process.env.PORT}`);
 })
